1572a887f2329563bba787981b96fc1656fb2520
Howard <reedless@DESKTOP-RHH1G98.localdomain>
2021-09-29 15:12:18 +0800

    todo refresh, persist state, validate input, and delete

--- frontend/src/screens/Todo.tsx
+++ frontend/src/screens/Todo.tsx
@@ -19,24 +19,41 @@ export type TodoItemProps = {
 };
 
 function TodoItem(props: TodoItemProps) {
-  /* create state here */
+  const [done, setDone] = useState(props.done);
 
   const updateTodoItem = useCallback(async () => {
     await axios.put(`${CONFIG.API_ENDPOINT}/todos/${props.id}`, {
       id: props.id,
       description: props.description,
-      /* persist the state of the todo item */
+      done: done,
     });
-  }, [props.description, props.id]);
+  }, [props.description, props.id, done]);
+
+  const deleteTodoItem = useCallback(async () => {
+    const res = await axios.delete(`${CONFIG.API_ENDPOINT}/todos/${props.id}`);
+    console.log(res);
+    alert(`The todo ${props.description} has been deleted. Please refresh the list.`);
+  }, [props.id, props.description]);
 
   useEffect(() => {
-    /* mark the todo when done (as a dependency) changes */
-  }, [props.description, updateTodoItem]);
+    console.log(props)
+    updateTodoItem();
+  }, [props, updateTodoItem]);
 
   return (<>
     <tr>
-      <td>{/* insert checkbox here */}</td>
+      <td>
+        <input type="checkbox"
+          onChange={(event) => setDone(event.currentTarget.checked)}
+          checked={done}>
+        </input>
+      </td>
       <td width={'100%'}>{props.description}</td>
+      <td>
+        <Button type="button">
+          <span className='sgds-icon sgds-icon-delete' onClick={deleteTodoItem}/>
+        </Button>
+      </td>
     </tr>
   </>
   );
@@ -49,6 +66,7 @@ interface TodoProps {
 function Todo(props: TodoProps) {
   const [todoItems, setTodoItems] = useState<{ [id: string]: TodoItemProps }>({});
   const [newTodoDescription, setNewTodoDescription] = useState('');
+  const [isRefresh, setIsRefresh] = useState(false);
 
   const populateTodos = useCallback(async () => {
     const result = await axios.get(`${CONFIG.API_ENDPOINT}/todos`);
@@ -57,7 +75,12 @@ function Todo(props: TodoProps) {
 
   const onRefreshClicked = useCallback(async () => {
     console.log('Refresh button clicked');
-    /* refresh todos here */
+    setIsRefresh(true);
+    setTimeout(async () => {
+      await populateTodos();
+      setIsRefresh(false);
+      console.log('todoList updated');
+    }, 400);
   }, [populateTodos]);
 
   useEffect(() => {
@@ -65,13 +88,17 @@ function Todo(props: TodoProps) {
   }, [onRefreshClicked]);
 
   async function submitNewTodo() {
-    /* validate todos here */
-    const newTodo = {
-      description: newTodoDescription,
-    };
-    await axios.post(`/api/todos`, newTodo);
-    await populateTodos();
-    setNewTodoDescription('');
+    if (newTodoDescription.trim() !== "") {
+      const newTodo = {
+        description: newTodoDescription.trim(),
+      };
+      await axios.post(`/api/todos`, newTodo);
+      await populateTodos();
+      setNewTodoDescription('');
+      alert(`${newTodoDescription.trim()} has been added as a new todo.`);
+    } else {
+      alert("Invalid todo. Todo is either empty or just whitespace.")
+    }
   }
 
   return (
@@ -98,7 +125,9 @@ function Todo(props: TodoProps) {
                       <Button isPrimary isLoading={false}>Submit</Button>
                     </Col>
                     <Col>
-                      {/* insert button here */}
+                      <Button type="button" isLoading={isRefresh} onClick={onRefreshClicked}>
+                        <span className='sgds-icon sgds-icon-refresh' />
+                      </Button>
                     </Col>
                   </Row>
                 </div>
@@ -107,7 +136,7 @@ function Todo(props: TodoProps) {
           </Section>
           <Section isSmall>
             <Table isFullwidth isHoverable isHorizontal isBordered>
-              <thead><tr><th>Done</th><th>Description</th></tr></thead>
+              <thead><tr><th>Done</th><th>Description</th><th>Delete</th></tr></thead>
               <tbody>
                 {
                   Object.keys(todoItems).map((item) => (<TodoItem key={todoItems[item].id} {...todoItems[item]} />))
